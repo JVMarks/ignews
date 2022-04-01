b0cca6219eba6eeb1b9bb72cadbd72ed01dfdda6
João Victor <jovic.marks@gmail.com>
2022-04-01 11:47:14 -0300

    webhooks striper

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from 'next-auth/client';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface ISubscribeButtonProps {
@@ -5,11 +10,33 @@ interface ISubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: ISubscribeButtonProps) {
+  const [session] = useSession();
+
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
